4fbf19c883413e4832671cbd487a613fc4381099
Lit-bit0926 <as26149@naver.com>
2023-05-16 10:43:56 +0900

    '클래스나누기및함수객체화'

--- 송용재/backend13/src/practice/Combine.java
+++ 송용재/backend13/src/practice/Combine.java
@@ -3,6 +3,10 @@ package practice;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//패키지 하위 파일 임포트 
+import practice.Calculation;
+import practice.Multiple;
+
 public class Combine {
 
 	public static void main(String[] args) {
@@ -21,12 +25,11 @@ public class Combine {
 				// 예외처리를 위한 try-catch 문 선언
 				try {
 					num1 = sc.nextInt(); // 입력받은 숫자를 num1 변수에 저장한다.
+					Multiple mul = new Multiple(num1);
 					if ((num1 > 999) || (num1 < 1)) // 입력받은 숫자가 조건의 범위를 만족하지 않을경우 안내 메시지 출력
 						System.out.println("1~999 사이의 숫자를 입력해주세요 ");
 					else {
-						for (int i = 1; i <= 9; i++) { // 조건 만족 시 구구단 실행
-							System.out.println(num1 + " * " + i + " = " + num1 * i);
-						}
+						mul.showGugudan(num1);
 					}
 				} catch (InputMismatchException e) {
 					// 숫자가 아닌 문자를 입력할 경우, 에러메시지 출력
@@ -34,34 +37,31 @@ public class Combine {
 				}
 			} else if (input == 2) { // 계산기 실행
 				System.out.println("숫자 2개를 입력해주세요 ");
-				int cNum1 = sc.nextInt();
-				int cNum2 = sc.nextInt();
-				int res = 0;
+				double cNum1 = sc.nextInt();
+				double cNum2 = sc.nextInt();
+				
+				Calculation cal = new Calculation(cNum1, cNum2);
 
 				System.out.println("하고자 하는 연산을 입력해주세요(+, -, *, /) : ");
-				char cal;
-				cal = sc.next().charAt(0); // 문자를 저장하는 것은 없어서 라인을 저장한 후 첫번째 문자를 저장
+				char op;
+				op = sc.next().charAt(0); // 문자를 저장하는 것은 없어서 라인을 저장한 후 첫번째 문자를 저장
 
-				if (cal == '+') {
-					res = cNum1 + cNum2;
-					System.out.println(cNum1 + " + " + cNum2 + " = " + res);
-				} else if (cal == '-') {
-					if (cNum1 < cNum2) {
-						System.out.println("2번째 숫자가 더 큽니다.다시 입력해주세요");	// 결과가 음수가 나올 시 안내메시지
-					} else {
-						res = cNum1 - cNum2;
-						System.out.println(cNum1 + " - " + cNum2 + " = " + res);
-					}
-				} else if (cal == '*') {
-					res = cNum1 * cNum2;
-					System.out.println(cNum1 + " * " + cNum2 + " = " + res);
-				} else if (cal == '/') {
-					if (cNum2 == '0') {
-						System.out.println("나누는 숫자는 0이 될 수 없습니다.");
-					} else {
-						res = cNum1 / cNum2;
-						System.out.println(cNum1 + " / " + cNum2 + " = " + res);
-					}
+				switch (op) {
+				case '+':
+					cal.calPlus(cNum1, cNum2);
+					break;
+				case '-':
+					cal.calMinus(cNum1, cNum2);
+					break;
+				case '*':
+					cal.calMulti(cNum1, cNum2);
+					break;
+				case '/':
+					cal.calDivide(cNum1, cNum2);
+					break;
+				default:
+					System.out.println("지원하지 않는 연산입니다.");
+					break;
 				}
 
 			} else if (input == 3) {
